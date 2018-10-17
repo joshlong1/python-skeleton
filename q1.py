8959d3c0e35e5a63efa076765ea4760658f490d2
joshlong1 <43318732+joshlong1@users.noreply.github.com>
2018-10-17 15:37:17 +0100

    Update q1.py

--- q1.py
+++ q1.py
@@ -4,6 +4,17 @@ import numpy as np
 
 # modify this function, and create other functions below as you wish
 def question01(portfolios):
-  # modify and then return the variable below
-  answer = 0
-  return answer
+    Max=2**4
+    pairs=[]
+    while(Max>0):
+        Max-=1
+        for i in portfolios:
+            p=i^Max
+            pairs.append(p)
+        for x in pairs:
+            for y in portfolios:
+                if x==y:
+                    answer = Max
+                    print(x)
+                    print(x^Max)
+                    return answer
