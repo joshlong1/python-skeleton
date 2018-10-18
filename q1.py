54dda211e39a8a29d292ea778c6be86e86880f9f
joshlong1 <43318732+joshlong1@users.noreply.github.com>
2018-10-18 13:43:46 +0100

    Update q1.py

--- q1.py
+++ q1.py
@@ -4,12 +4,12 @@ import numpy as np
 
 # modify this function, and create other functions below as you wish
 def question01(portfolios):
-    x=range(len(portfolios))
+    
     xor=[]
 
-    for i in x:  
-        for j in x:
-            a=portfolios[i]^portfolios[j]
+    for i in portfolios:  
+        for j in portfolios:
+            a=i^j
             xor.append(a)
             
     answer=max(xor)
