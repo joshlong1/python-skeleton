7dda6773aeb2980c297c19df9125461bfd66e2f1
joshlong1 <43318732+joshlong1@users.noreply.github.com>
2018-10-21 22:37:02 +0100

    Update q2.py

--- q2.py
+++ q2.py
@@ -4,10 +4,11 @@ import numpy as np
 
 #compute all combinations for two portfolios
 def question02(cashFlowIn, cashFlowOut):
-    MaxIn=sum(cashFlowIn)
+    MaxIn=max(sum(cashFlowIn),sum(cashFlowOut))
 
     lenIn=len(cashFlowIn)
     lenOut=len(cashFlowOut)
+
     
     subIn=[]
     subOut=[]
@@ -25,9 +26,7 @@ def question02(cashFlowIn, cashFlowOut):
                 subIn[i,j]=subIn[i,j-1]
             if (i>=cashFlowIn[j-1]):
                 subIn[i,j]=subIn[i-cashFlowIn[j-1],j-1] or subIn[i,j-1]
-                
-                 
-             
+                           
     for i in range(MaxIn+1):
         subOut.append([0])
         for j in range(lenOut):
@@ -42,25 +41,18 @@ def question02(cashFlowIn, cashFlowOut):
             if (i>=cashFlowOut[j-1]):
                 subOut[i,j]=subOut[i-cashFlowOut[j-1],j-1] or subOut[i,j-1]
 
-    Diff=0
+
     subIn=subIn[1:,lenIn]
     subOut=subOut[1:,lenOut]
     match=0
-    cashFlowIn=np.array(cashFlowIn)
-    Srt=np.min(cashFlowIn[np.nonzero(cashFlowIn)])
+    answer=min(cashFlowIn+cashFlowOut)
     
-    for j in range(Srt):
-        for i in range(MaxIn-1):
-            match=subIn[i] and subOut[i]
-            
-            if (match==1):
-                answer=Diff
-                return answer
+    for i in range(len(subIn)):
+        for j in range(len(subOut)):
+        
+            if (subIn[i] and subOut[j]):
+                a=abs(i-j)
+                if a<answer:
+                    answer=a
     
-        subIn=subIn[1:]
-        subIn=np.append(subIn,[0])
-        Diff+=1
-    
-    
-    answer=Srt
     return answer
