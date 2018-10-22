2738d39556f05be7202960dc125a4c052074702d
joshlong1 <43318732+joshlong1@users.noreply.github.com>
2018-10-22 13:41:23 +0100

    Update q6.py

--- q6.py
+++ q6.py
@@ -4,5 +4,13 @@ import numpy as np
 
 def question06(numServers, targetServer, times):
   # modify and then return the variable below
-  answer = -1
-  return answer
+    times=np.array(times)
+    for node in times:
+        for t in range(len(node)):
+            dist=node[0]+node[t]
+            if dist<times[t,0]:
+                times[t,0]=dist
+            
+    print times
+    answer = times[targetServer,0]
+    return answer
