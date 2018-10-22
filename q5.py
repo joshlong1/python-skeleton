ef2ca5fb02a65bfc5c46105d218c09ccbc2d8a87
joshlong1 <43318732+joshlong1@users.noreply.github.com>
2018-10-22 10:05:40 +0100

    Update q5.py

--- q5.py
+++ q5.py
@@ -3,6 +3,16 @@
 import numpy as np
 
 def question05(allowedAllocations, totalValue):
-  # modify and then return the variable below
-  answer = -1
-  return answer
+    if (totalValue==0):
+        return 0
+    answer=minAllocation(allowedAllocations,totalValue)
+    return answer
+    
+def minAllocation(alloc,tot):
+    res=tot
+    for c in alloc:
+        if (c<=tot):
+            sub_res=minAllocation(alloc,tot-c)
+            if sub_res+1<res:
+                res=sub_res+1
+    return res
