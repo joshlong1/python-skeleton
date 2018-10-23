ec66eba77cd5df26322e98d530a6d6ebaf472f5c
joshlong1 <43318732+joshlong1@users.noreply.github.com>
2018-10-23 09:50:38 +0100

    Update q5.py

--- q5.py
+++ q5.py
@@ -7,23 +7,21 @@ def question05(allowedAllocations, totalValue):
     if totalValue<=0:
         return 0
     
-    minAlloc=np.zeros(totalValue+1)
-    allowedAllocations=sorted(allowedAllocations,reverse=1)
+    minAlloc=[0 for i in range(totalValue+1)]
+    #allowedAllocations=sorted(allowedAllocations,reverse=1)
     
     for i in allowedAllocations:
         try: minAlloc[i]=1
         except: continue
     
-    if minAlloc[totalValue]==1: return 1
+    #if minAlloc[totalValue]==1: return 1
         
-    for k in range(allowedAllocations[-1],totalValue+1):
-        for j in allowedAllocations:
-            if k-j>0:
-                a=minAlloc[k-j]
-                if 1<a+1<minAlloc[k] or minAlloc[k]==0:
-                    minAlloc[k] = a+1
-                    if minAlloc[totalValue]==2:return 2
+    for k in range(min(allowedAllocations),totalValue+1):
+        for j in [c for c in allowedAllocations if c <= k]:
+            if minAlloc[k-j]>0:
+                if minAlloc[k-j]+1<minAlloc[k] or minAlloc[k]==0:
+                    minAlloc[k] = minAlloc[k-j]+1
 
     
-    answer=int(minAlloc[totalValue])
+    answer=minAlloc[totalValue]
     return answer
