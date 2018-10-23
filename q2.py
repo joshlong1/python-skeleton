3ab98204cd181892e87b60807269da06a2ce7770
joshlong1 <43318732+joshlong1@users.noreply.github.com>
2018-10-23 19:15:49 +0100

    Update q2.py

--- q2.py
+++ q2.py
@@ -5,28 +5,26 @@ import numpy as np
 #compute all combinations for two portfolios
 def question02(cashFlowIn, cashFlowOut):
 
-    setIn=[sum(x) for x in powerset(cashFlowIn)]
-    setOut=[sum(y) for y in powerset(cashFlowOut)]
+    setIn={sum(x) for x in powerset(cashFlowIn)}
+    setOut={sum(y) for y in powerset(cashFlowOut)}
     answer=1000*100
  
     for i in setIn:
         for j in setOut:
-            diff=abs(i-j)
             if i+j==0:
-                continue
-            if diff<answer:
-                answer=diff
-                if answer==0:
-                    return answer
+                diff=abs(i-j)
+                if diff<answer:
+                    answer=diff
+                    if answer==0:
+                        return answer         
     return answer
     
 def powerset(seq):
-
-    if len(seq) <= 1:
-        yield seq
+    if seq == []:
         yield []
     else:
-        for item in powerset(seq[1:]):
-            yield [seq[0]]+item
-            yield item
+        a = seq[0]
+        for tail in powerset(seq[1:]):
+            yield tail
+            yield [a] + tail
         
