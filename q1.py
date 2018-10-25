eafbd9e18bc821352e9f419878cc4dcc15a742a7
joshlong1 <43318732+joshlong1@users.noreply.github.com>
2018-10-25 10:51:54 +0100

    Update q1.py

--- q1.py
+++ q1.py
@@ -5,14 +5,9 @@ import numpy as np
 # modify this function, and create other functions below as you wish
 def question01(portfolios):
     
-    xor=[]
-    answer=0
-    for i in range(len(portfolios)):
-        b=portfolios[0]
-        portfolios.remove(b)
-        for j in portfolios:
-            a=b^j
-            if a>answer:
-                answer=a
-          
+    answer = 0
+    for i in range(17)[::-1]:
+        answer <<= 1
+        prefixes = {num >> i for num in portfolios}
+        answer += any(answer^1 ^ p in prefixes for p in prefixes)
     return answer
