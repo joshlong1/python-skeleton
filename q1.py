5a26edafcb8bc14fadb3b11bb4c42d0e3e7dd48a
joshlong1 <43318732+joshlong1@users.noreply.github.com>
2018-10-27 08:23:48 +0100

    Update q1.py

--- q1.py
+++ q1.py
@@ -5,9 +5,14 @@ import numpy as np
 # modify this function, and create other functions below as you wish
 def question01(portfolios):
     
-    if len(portfolios)<2 or max(portfolios)==0:
-        answer=0
-        return answer
-    answer=max([portfolios[a]^portfolios[b] for a in range(len(portfolios)) for b in range(a+1, len(portfolios))])
+    xor=[]
+    answer=0
+    L=len(portfolios)
 
+    for i in range(L):
+        for j in range(i+1,L):
+            a=portfolios[i]^portfolios[j]
+            if a>answer:
+                answer=a
+          
     return answer
