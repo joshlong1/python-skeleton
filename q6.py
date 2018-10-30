65d05ad877c928385d7250ecfc4c2d8025a16a44
joshlong1 <43318732+joshlong1@users.noreply.github.com>
2018-10-30 12:16:37 +0000

    Update q6.py

--- q6.py
+++ q6.py
@@ -5,10 +5,11 @@ import numpy as np
 def question06(numServers, targetServer, times):
   # modify and then return the variable below
     for node in times:
-        for t in range(len(node)):
-            dist=node[0]+node[t]
-            if dist<times[t][0]:
-                times[t][0]=dist
+        if min(node)<times[targetServer][0]:
+            for t in range(len(node)):
+                dist=node[0]+node[t]
+                if dist<times[t][0]:
+                    times[t][0]=dist
             
     answer = times[targetServer][0]
     return answer
