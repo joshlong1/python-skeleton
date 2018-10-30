befdfcc36a0d756356871d8d9b9e1df12248b71d
joshlong1 <43318732+joshlong1@users.noreply.github.com>
2018-10-30 16:22:09 +0000

    Update q4.py

--- q4.py
+++ q4.py
@@ -4,19 +4,18 @@ import numpy as np
 
 #compute all combinations for two portfolios
 def question04(rows, numberMachines):
-    X='X'
+
     answer=0
     for r in rows:
         window=[]
         for j in range(len(r)):
-            if r[j]==X:
+            if r[j]=="X":
                 window=[]
             else:
                 window+=[int(r[j])]
-                if len(window)==numberMachines:
+                if (len(window)==numberMachines):
                     ans=sum(window)
-                    if ans<answer or answer==0: 
-                        answer=ans
+                    answer=ans if answer==0 else min(ans,answer)
                     window=window[1:]
                     
     return answer
