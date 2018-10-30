cb9252805fd978a35150fcddb0a51afa3b48fa24
joshlong1 <43318732+joshlong1@users.noreply.github.com>
2018-10-30 12:26:46 +0000

    Update q4.py

--- q4.py
+++ q4.py
@@ -6,16 +6,9 @@ import numpy as np
 def question04(rows, numberMachines):
     
     X='X'
-    answer=0
-    for i in rows:
-        if len(i)>=numberMachines:  
-            for j in range(len(i)-numberMachines+1):
-                nums=i[j:(j+numberMachines)]
-                try:
-                    nums=sum(map(int, nums))
-                    if nums<answer or answer==0: 
-                        answer=nums
-                except:
-                    continue
-
+    sums=[sum(i[j:(j+numberMachines)]) for i in rows for j in range(len(i)-numberMachines+1) if X not in i[j:(j+numberMachines)]]
+    if sums==[]:
+        return 0
+    answer=min(sums)
+    
     return answer
