81152b2503218ef81134d37e01476a4bf0e0760d
joshlong1 <43318732+joshlong1@users.noreply.github.com>
2018-10-30 17:47:36 +0000

    Update q6.py

--- q6.py
+++ q6.py
@@ -3,19 +3,12 @@
 import numpy as np
 
 def question06(numServers, targetServer, times):
-    D=[0]+["inf"]*(numServers-1)
-    v=range(numServers)
-    visited=[]
-    j=0
-    while len(v)>0:
-        visit = D.index(sorted(D)[j])
-        j+=1
-        v.remove(visit)
-        
-        for i in v:
-            D[i]=min(D[visit]+times[visit][i],D[i])
-
-                
-    answer=D[targetServer]
+  # modify and then return the variable below
+    for node in times:
+        for t in range(len(node)):
+            dist=node[0]+node[t]
+            times[t][0]=min(dist, times[t][0])
+            
+    answer = times[targetServer][0]
     return answer
             
