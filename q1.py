bdd3a9b757ecaa73cddd680547683c1de8e3acd4
joshlong1 <43318732+joshlong1@users.noreply.github.com>
2018-10-31 14:18:12 +0000

    Update q1.py

--- q1.py
+++ q1.py
@@ -5,6 +5,7 @@ import numpy as np
 # modify this function, and create other functions below as you wish
 def question01(portfolios):
     answer=0
+    portfolios=list(set(portfolios))
     L=len(portfolios)
     for i in range(L-1):
         for j in range(i+1,L):
