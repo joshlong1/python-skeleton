93725e80a866c46b6b4e31aa231fa76b1594a1a5
joshlong1 <43318732+joshlong1@users.noreply.github.com>
2018-10-31 15:47:48 +0000

    Update q1.py

--- q1.py
+++ q1.py
@@ -6,9 +6,20 @@ import numpy as np
 def question01(portfolios):
     answer=0
     portfolios=list(set(portfolios))
-    L=len(portfolios)
-    for i in range(L-1):
-        for j in range(i+1,L):
-            answer=max(portfolios[i]^portfolios[j],answer)
+    portfolios=sorted(portfolios,reverse=True)
+    bitLengths=[M.bit_length() for M in portfolios]
+    L=bitLengths.count(bitLengths[0])
+    if L<len(portfolios):
+        for i in portfolios[:L]:
+            for j in portfolios[L:]:
+                a=i^j
+                if a>answer:
+                    answer=a
+    else:
+        for i in range(L-1):
+            for j in range(i+1,L):
+                a=portfolios[i]^portfolios[j]
+                if a>answer:
+                    answer=a
 
     return answer
