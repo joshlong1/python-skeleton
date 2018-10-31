d0a90c459f4974477b6df6fbf9d331ce80159b9c
joshlong1 <43318732+joshlong1@users.noreply.github.com>
2018-10-31 22:02:13 +0000

    Update q5.py

--- q5.py
+++ q5.py
@@ -4,18 +4,32 @@ import numpy as np
 
 def question05(allowedAllocations, totalValue):
     
-    if totalValue<=0:
-        return 0
-
     allowedAllocations=list({x for x in allowedAllocations if x!=0 and x<=totalValue})
     if len(allowedAllocations)==0:
         return 0
-    minAlloc=[0]+[10*totalValue]*(totalValue)
-        
-    for k in range(min(allowedAllocations),totalValue+1):
-        for j in [c for c in allowedAllocations if c <= k]:
-                minAlloc[k] = min(minAlloc[k-j]+1, minAlloc[k]) 
-
-    answer=0 if minAlloc[totalValue]>9*totalValue else minAlloc[totalValue]
+    maxAlloc=max(allowedAllocations)
     
-    return answer
+    if totalValue <=0 or totalValue < min(allowedAllocations):
+
+        return 0
+    
+    elif totalValue%maxAlloc in allowedAllocations:
+
+        return totalValue/maxAlloc + 1
+
+    elif totalValue%maxAlloc == 0:
+
+        return totalValue/maxAlloc
+
+    else:
+
+        
+        minAlloc=[0]+[10*totalValue]*(totalValue)
+        
+        for k in range(min(allowedAllocations),totalValue+1):
+            for j in [c for c in allowedAllocations if c <= k]:
+                    minAlloc[k] = min(minAlloc[k-j]+1, minAlloc[k]) 
+
+        answer=0 if minAlloc[totalValue]>9*totalValue else minAlloc[totalValue]
+    
+        return answer
