0b02e68c979d563796fb45528a82da95be54ef7e
joshlong1 <43318732+joshlong1@users.noreply.github.com>
2018-10-31 11:35:02 +0000

    Update q1.py

--- q1.py
+++ q1.py
@@ -4,15 +4,12 @@ import numpy as np
 
 # modify this function, and create other functions below as you wish
 def question01(portfolios):
-        result = 0
-        for i in reversed(xrange(16)):
-            result <<= 1
-            prefixes = set()
-            for n in portfolios:
-                prefix.add(n >> i)
-            for p in prefix:
-                if (result | 1) ^ p in prefix:
-                    result += 1
-                    break
+    answer=0
+    L=len(portfolios)
+    for i in range(L-1):
+        for j in range(i+1,L):
+            answer=max(portfolios[i]^portfolios[j],answer)
+            if answer==2**16-1:
+                return answer
 
-        return result
+    return answer
