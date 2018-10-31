e7956ddec7c3059fe58706308979ae81f9d50da9
joshlong1 <43318732+joshlong1@users.noreply.github.com>
2018-10-31 21:50:16 +0000

    Update q1.py

--- q1.py
+++ q1.py
@@ -5,18 +5,19 @@ import numpy as np
 # modify this function, and create other functions below as you wish
 def question01(portfolios):
     answer=0
-    if len(portfolios)<2:
-        return 0
     portfolios=list(set(portfolios))
     portfolios=sorted(portfolios,reverse=True)
     bitLengths=[M.bit_length() for M in portfolios]
-    L=bitLengths.count(bitLengths[0])
+    maxBits=bitLengths[0]
+    L=bitLengths.count(maxBits)
     if L<len(portfolios):
         for i in portfolios[:L]:
             for j in portfolios[L:]:
                 a=i^j
                 if a>answer:
                     answer=a
+                if a==2**(maxBits+1)-1:
+                    return a
     else:
         for i in range(L-1):
             for j in range(i+1,L):
